f9a5c59ed4aff701b5cb8302d44a822f072d06a7
McNiel Viray <mcniel.viray@yahoo.com>
2021-02-18 19:30:31 +0800

    created commands,core,domain,mappers,database

---
+++ MVSolution/MNV.Commands/User/CreateUserCommand.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using MediatR;
+using MNV.Core.Database;
+using MNV.Core.Exceptions;
+using MNV.Domain.Constants;
+using MNV.Domain.Models.Responses;
+using MNV.Domain.Models.Responses.User;
+using MNV.Domain.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MNV.Commands.User
+{
+    /// <summary>
+    /// It handles our Query, handler .Response is in Domain.
+    /// We put those three in container for code discoverability purpose.
+    /// 
+    /// Benefits of using records is its immutable
+    /// </summary>
+    public static class CreateUserCommand
+    {
+        #region Command
+        public class Command : ICommand
+        {
+            public Command(UserViewModel model)
+            {
+                Model = model;
+            }
+            public UserViewModel Model { get; set; }
+        }
+        #endregion
+
+        #region Handler
+        public class Handler : CommandHandler, IRequestHandler<Command, ICommandResponse>
+        {
+            public Handler(IDataContext dataContext, IMapper mapper) : base(dataContext, mapper)
+            {
+            }
+            public async Task<ICommandResponse> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var data = _mapper.Map<Domain.Entities.User>(request.Model);
+                _dataContext.User.Add(data);
+                await _dataContext.SaveChangesAsync()
+                    .ConfigureAwait(false);
+
+                if (data == null)
+                    throw new EntityNotCreatedException(ExceptionMessageConstants.ErrorCreatingUser);
+
+                var result = _mapper.Map<UserViewModel>(data);
+
+                return new CreateUserResponse(data.ID, result);
+            }
+        }
+        #endregion
+    }
+}
---
+++ MVSolution/MNV.Domain/Models/Responses/User/CreateUserResponse.cs
@@ -0,0 +1,18 @@
+﻿using MNV.Domain.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MNV.Domain.Models.Responses.User
+{
+    public class CreateUserResponse : ICommandResponse
+    {
+        public CreateUserResponse(long id, UserViewModel user)
+        {
+            ID = id;
+            User = user;
+        }
+        public long ID { get; set; }
+        public UserViewModel User { get; set; }
+    }
+}
